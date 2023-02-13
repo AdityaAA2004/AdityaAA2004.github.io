5190683e3f9477bd355288e6c8ac2d1dbc0e7fa2
Aditya Archunan Anand <anandad1@msu.edu>
2023-02-13 12:59:38 -0600

    Modifications for visibility

--- common_script.js
+++ common_script.js
@@ -1,3 +1,21 @@
 var a = window.matchMedia("(prefers-color-scheme:dark)");
 console.log(a.matches);
+function toggle_background(){
+    var color = $("body").css("background-color");
+    var parts = color.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    delete (parts[0]);
+    for (var i = 1; i <= 3; ++i) {
+        parts[i] = parseInt(parts[i]).toString(16);
+        if (parts[i].length == 1) parts[i] = '0' + parts[i];
+    } 
+    var hexString ='#'+parts.join('').toUpperCase();
+    if (hexString == "#000000"){
+        $("body").css("background-color","#BFE0E3"); 
+        $("body").css("color","black");
+    }
+    else{
+        $("body").css("background-color","#000000"); 
+        $("body").css("color","white");
+    }
+}
 
